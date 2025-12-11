27b42bcc00bb387a19a76d1de5913a9c5ef9ee13
felix <ffelixrichardo@gmail.com>
2025-12-11 19:09:33 +0700

    auth controller done

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import { loginSchema, registerSchema } from "./auth.validation";
+import { AuthService } from "./auth.service";
 
 export class AuthController{
 
     static async login(req: Request, res: Response, next: NextFunction){
         try{
-            
+            const validatedData = loginSchema.parse(req.body);
+            const token = await AuthService.login(validatedData);
+            res.status(200).json({
+                message: "Login succesfully",
+                data: token
+            });
         }catch(err){
             next(err);
         }
@@ -12,7 +19,12 @@ export class AuthController{
     
     static async register(req: Request, res: Response, next: NextFunction){
         try{
-            
+            const validatedData = registerSchema.parse(req.body);
+            const token = await AuthService.register(validatedData);
+            res.status(201).json({
+                message: "Register succesfully",
+                data: token
+            });
         }catch(err){
             next(err);
         }
