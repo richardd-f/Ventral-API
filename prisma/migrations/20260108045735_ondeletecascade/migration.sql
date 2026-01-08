9a1fa60e873817ac1febc41ac424eee46a7df721
felix <ffelixrichardo@gmail.com>
2026-01-08 12:01:21 +0700

    add on delete cascade to imageevent

---
+++ prisma/migrations/20260108045735_ondeletecascade/migration.sql
@@ -0,0 +1,16 @@
+-- AlterEnum
+ALTER TYPE "UserVerificationStatus" ADD VALUE 'PENDING';
+COMMIT;
+
+-- DropForeignKey
+ALTER TABLE "ImageEvent" DROP CONSTRAINT "ImageEvent_event_id_fkey";
+
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "city" TEXT;
+
+-- AlterTable
+ALTER TABLE "UserVerification" ALTER COLUMN "status" SET DEFAULT 'PENDING';
+
+-- AddForeignKey
+ALTER TABLE "ImageEvent" ADD CONSTRAINT "ImageEvent_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "Event"("event_id") ON DELETE CASCADE ON UPDATE CASCADE;
